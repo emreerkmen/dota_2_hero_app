488e42d49983931141bbeb39b133aafebe75443e
emreerkmen <emre.erkmen@hotmail.com>
2020-07-15 19:04:29 +0300

    much more tight purple is added to light theme.

--- lib/final/theme_final.dart
+++ lib/final/theme_final.dart
@@ -16,9 +16,9 @@ final ThemeData lightTheme = ThemeData.light().copyWith(
   primaryColor: Color(0xfff5f5f5),
   accentColor: Color(0xff40bf7a),
   textTheme: TextTheme(
-      headline3: TextStyle(color: Color(0xffCFCBD9)),
-      headline4: TextStyle(color: Color(0xffCFCBD9)),
-      headline6: TextStyle(color: Color(0xffCFCBD9)),
+      headline3: TextStyle(color: Color(0xff857e96)),
+      headline4: TextStyle(color: Color(0xff857e96)),
+      headline6: TextStyle(color: Color(0xff857e96)),
       subtitle2: TextStyle(color: Colors.grey),
       subtitle1: TextStyle(color: Colors.white)),
   appBarTheme: AppBarTheme(
