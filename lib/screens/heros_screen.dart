4cf20fe4efffcb2526d3682529bad1ff1e6d012c
emreerkmen <emre.erkmen@hotmail.com>
2020-07-15 17:43:37 +0300

    Change Theme button set to the bottom.

--- lib/screens/heros_screen.dart
+++ lib/screens/heros_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:dota_2_hero_app/model/theme_model.dart';
 
-
 class HerosScreen extends StatelessWidget {
   const HerosScreen({Key key}) : super(key: key);
 
@@ -10,15 +9,30 @@ class HerosScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: FlatButton(
-            onPressed: () {
-              context.read<ThemeModel>().toggleTheme();
-            },
-            child: Text(
-              "Change Theme",
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: Container(),
             ),
-          ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                child: Row(
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        context.read<ThemeModel>().toggleTheme();
+                      },
+                      child: Text(
+                        "Change Theme",
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
